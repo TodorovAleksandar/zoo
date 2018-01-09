3fff0a11e5256eb2d9e3346b93eec93249c208e3
aleksandar <todor.petrovgrad@yahoo.com>
2018-01-09 21:08:39 +0100

    3 commit

---
+++ src/app/animal.ts
@@ -0,0 +1,14 @@
+
+
+export class Animal {
+    vrsta:string;
+    ime:string;
+    datum_rodjenja:string;
+    
+
+    constructor(vrsta: string, ime: string,datum_rodjenja: string){
+    this.vrsta = vrsta;
+    this.ime = ime;
+    this.datum_rodjenja = datum_rodjenja;
+}
+}
